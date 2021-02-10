f2568613a60c0831854973f21d1ddb39a14e0dc1
Théo Cauët <42837832+cauettheo@users.noreply.github.com>
2021-02-10 14:00:08 +0100

    [+] first card list with simple images

---
+++ lib/class/CardItem.dart
@@ -0,0 +1,22 @@
+class CardItem {
+  String name;
+  String manaCost;
+  double cmc;
+  List<String> colors;
+  List<String> colorIdentity;
+  String type;
+  List<String> supertypes;
+  List<String> types;
+  List<String> subtypes;
+  String rarity;
+  String set;
+  String setName;
+  String text;
+  String artist;
+  String number;
+  String power;
+  String toughness;
+  String layout;
+  String id;
+  String imageUrl;
+}
--- lib/tabs/MyCollection.dart
+++ lib/tabs/MyCollection.dart
@@ -1,23 +1,28 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:MagicFlutter/data.dart';
 
 class MyCollectionView extends StatefulWidget {
-  MyCollectionView({
-    Key key,
-  }) : super(key: key);
-
   @override
   _MyCollectionViewState createState() => _MyCollectionViewState();
 }
 
 class _MyCollectionViewState extends State<MyCollectionView> {
-
   @override
   Widget build(BuildContext context) {
+    print(cardList[0]["imageUrl"]);
     return Container(
         child: ListView.builder(
-          itemCount: 0,
+          itemCount: cardList == null ? 0 : cardList.length,
           itemBuilder: (BuildContext context, int index) {
-            return;
+            return new Image(
+              image: NetworkImage("https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" + cardList[index]['identifiers']['multiverseId']),
+            );
           },
         ));
   }
