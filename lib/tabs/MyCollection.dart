4e82c82aa4fbce69d17ddc47e3c2bc0e8e0138fa
Théo Cauët <42837832+cauettheo@users.noreply.github.com>
2021-02-10 15:16:13 +0100

    [+] ActionItem widget with menu and on tap

--- lib/tabs/MyCollection.dart
+++ lib/tabs/MyCollection.dart
@@ -1,11 +1,6 @@
-import 'dart:io';
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:MagicFlutter/components/ActionItem.dart';
 import 'package:MagicFlutter/data.dart';
+import 'package:flutter/material.dart';
 
 class MyCollectionView extends StatefulWidget {
   @override
@@ -15,15 +10,92 @@ class MyCollectionView extends StatefulWidget {
 class _MyCollectionViewState extends State<MyCollectionView> {
   @override
   Widget build(BuildContext context) {
-    print(cardList[0]["imageUrl"]);
     return Container(
-        child: ListView.builder(
-          itemCount: cardList == null ? 0 : cardList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return new Image(
-              image: NetworkImage("https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" + cardList[index]['identifiers']['multiverseId']),
-            );
-          },
-        ));
+      child: ListView.builder(
+        itemCount: cardList == null ? 0 : cardList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ActionItem(
+            callback: () {
+              print(cardList[index]['name']);
+            },
+            item: Image(
+              image: NetworkImage(
+                  "https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" +
+                      cardList[index]['identifiers']['multiverseId']),
+            ),
+            menuCallbacks: [
+              () {
+                print("IOUI1");
+              },
+              () {
+                print("IOUI2");
+              },
+              () {
+                print("IOUI3");
+              },
+            ],
+            menuItems: <PopupMenuEntry>[
+              PopupMenuItem(
+                value: 0,
+                child: Wrap(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Icon(Icons.visibility),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text("See full screen"),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                value: 1,
+                child: Wrap(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Icon(Icons.delete),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text("Remove from collection"),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                value: 2,
+                child: Wrap(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Icon(Icons.add_circle),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text("Add to a deck"),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
