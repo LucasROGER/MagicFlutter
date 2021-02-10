449986a3c085ea631b05ffae81dc8b34b4859b2a
Théo Cauët <42837832+cauettheo@users.noreply.github.com>
2021-02-10 16:10:14 +0100

    [+] Dual list for cards

--- lib/tabs/MyCollection.dart
+++ lib/tabs/MyCollection.dart
@@ -1,4 +1,5 @@
 import 'package:MagicFlutter/components/ActionItem.dart';
+import 'package:MagicFlutter/components/DualList.dart';
 import 'package:MagicFlutter/data.dart';
 import 'package:flutter/material.dart';
 
@@ -11,89 +12,91 @@ class _MyCollectionViewState extends State<MyCollectionView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-        itemCount: cardList == null ? 0 : cardList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ActionItem(
-            callback: () {
-              print(cardList[index]['name']);
-            },
-            item: Image(
-              image: NetworkImage(
-                  "https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" +
-                      cardList[index]['identifiers']['multiverseId']),
-            ),
-            menuCallbacks: [
-              () {
-                print("IOUI1");
-              },
-              () {
-                print("IOUI2");
-              },
-              () {
-                print("IOUI3");
-              },
-            ],
-            menuItems: <PopupMenuEntry>[
-              PopupMenuItem(
-                value: 0,
-                child: Wrap(
-                  children: <Widget>[
-                    Row(
+      child: DualList<dynamic>(
+        list: cardList,
+        renderItem: (BuildContext context, int index, dynamic item) {
+          return Container(
+              padding: EdgeInsets.all(5),
+              child: ActionItem(
+                callback: () {
+                  print(item['name']);
+                },
+                item: Image(
+                  image: NetworkImage(
+                      "https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" +
+                          item['identifiers']['multiverseId']),
+                ),
+                menuCallbacks: [
+                  () {
+                    print(item['manaCost']);
+                  },
+                  () {
+                    print("2");
+                  },
+                  () {
+                    print("3");
+                  },
+                ],
+                menuItems: <PopupMenuEntry>[
+                  PopupMenuItem(
+                    value: 0,
+                    child: Wrap(
                       children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(Icons.visibility),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Icon(Icons.visibility),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text("See full screen"),
+                            )
+                          ],
                         ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Text("See full screen"),
-                        )
                       ],
                     ),
-                  ],
-                ),
-              ),
-              PopupMenuItem(
-                value: 1,
-                child: Wrap(
-                  children: <Widget>[
-                    Row(
+                  ),
+                  PopupMenuItem(
+                    value: 1,
+                    child: Wrap(
                       children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(Icons.delete),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Icon(Icons.delete),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text("Remove from collection"),
+                            )
+                          ],
                         ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Text("Remove from collection"),
-                        )
                       ],
                     ),
-                  ],
-                ),
-              ),
-              PopupMenuItem(
-                value: 2,
-                child: Wrap(
-                  children: <Widget>[
-                    Row(
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Wrap(
                       children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Icon(Icons.add_circle),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Icon(Icons.add_circle),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text("Add to a deck"),
+                            )
+                          ],
                         ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          child: Text("Add to a deck"),
-                        )
                       ],
                     ),
-                  ],
-                ),
-              )
-            ],
-          );
+                  )
+                ],
+              ));
         },
       ),
     );
