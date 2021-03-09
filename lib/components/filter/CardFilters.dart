bf81f874cdda3ca45b1347deb43fe98e67d8840d
Théo Cauët <42837832+cauettheo@users.noreply.github.com>
2021-03-09 11:03:02 +0100

    [*] style

--- lib/components/filter/CardFilters.dart
+++ lib/components/filter/CardFilters.dart
@@ -1,11 +1,11 @@
 import 'package:MagicFlutter/class/MagicCard.dart';
 import 'package:MagicFlutter/components/SearchBar.dart';
+import 'package:MagicFlutter/components/filter/ColorFilter.dart';
 import 'package:MagicFlutter/components/filter/Filter.dart';
 import 'package:MagicFlutter/components/filter/SetFilter.dart';
+import 'package:MagicFlutter/components/filter/TypeFilter.dart';
 import 'package:MagicFlutter/utils/ResponsiveSize.dart';
 import 'package:flutter/material.dart';
-import 'package:MagicFlutter/components/filter/ColorFilter.dart';
-import 'package:MagicFlutter/components/filter/TypeFilter.dart';
 import 'package:flutter/rendering.dart';
 
 import 'CmcFilter.dart';
@@ -46,8 +46,10 @@ class _CardFiltersState extends State<CardFilters> {
     List<MagicCard> res = [];
 
     for (int i = 0; i < widget.allCards.length; i++) {
-      if (allList.values.where((element) => element.contains(widget.allCards[i])).length == allList.length)
-        res.add(widget.allCards[i]);
+      if (allList.values
+              .where((element) => element.contains(widget.allCards[i]))
+              .length ==
+          allList.length) res.add(widget.allCards[i]);
       // res.addAll(allList[allList.keys.toList()[i]].where((element) => !res.contains(element)));
     }
 
@@ -68,8 +70,10 @@ class _CardFiltersState extends State<CardFilters> {
     for (int i = 0; i < widget.allCards.length; i++) {
       if (!costs.contains(widget.allCards[i].convertedManaCost))
         costs.add(widget.allCards[i].convertedManaCost);
-      colors.addAll(widget.allCards[i].colorIdentity.where((element) => !colors.contains(element)));
-      types.addAll(widget.allCards[i].types.where((element) => !types.contains(element)));
+      colors.addAll(widget.allCards[i].colorIdentity
+          .where((element) => !colors.contains(element)));
+      types.addAll(widget.allCards[i].types
+          .where((element) => !types.contains(element)));
       if (!sets.contains(widget.allCards[i].setCode))
         sets.add(widget.allCards[i].setCode);
     }
@@ -93,86 +97,123 @@ class _CardFiltersState extends State<CardFilters> {
 
   @override
   void didUpdateWidget(covariant CardFilters oldWidget) {
-    if (oldWidget != widget)
-      setup();
+    if (oldWidget != widget) setup();
     super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: FlatButton(
-            onPressed: () {
-              setState(() {
-                this.visible = !this.visible;
-              });
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                  image: this.visible ? AssetImage('assets/images/arrow_down.png') : AssetImage('assets/images/arrow_up.png'),
-                  width: ResponsiveSize.responsiveWidth(context, 5),
-                  height: ResponsiveSize.responsiveWidth(context, 5),
-                ),
-                Text(' Filters'),
-              ],
-            )
+    return Center(
+      child: Container(
+          // width: ResponsiveSize.responsiveWidth(context, 95),
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10.0),
+                bottomLeft: Radius.circular(10.0)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 4,
+                  blurRadius: 5,
+                  offset: Offset(0, 1))
+            ],
           ),
-        ),
-        Visibility(
-          visible: this.visible,
-            child: Column(
-              children: [
-                !widget.searchBar ? Container() : SearchBar(
-                  list: widget.allCards,
-                  updateFct: (List<dynamic> newList) {
-                    allList['search'] = newList.cast<MagicCard>();
-                    _update();
-                  },
-                ),
-                !widget.colors ? Container() : ColorFilter(
-                  colors: this.colors,
-                  list: widget.allCards,
-                  updateList: (List<dynamic> newList) {
-                    // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
-                    allList['colors'] = newList.cast<MagicCard>();
-                    _update();
-                  },
-                ),
-                  !widget.cmcs ? Container() : CmcFilter(
-                    costs: this.costs,
-                    list: widget.allCards,
-                    updateList: (List<dynamic> newList) {
-                      // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
-                      allList['cmc'] = newList.cast<MagicCard>();
-                      _update();
+          child: Column(
+            children: [
+              Visibility(
+                  visible: this.visible,
+                  child: Column(children: [
+                    !widget.searchBar
+                        ? Container()
+                        : SearchBar(
+                            list: widget.allCards,
+                            updateFct: (List<dynamic> newList) {
+                              allList['search'] = newList.cast<MagicCard>();
+                              _update();
+                            },
+                          ),
+                    !widget.cmcs
+                        ? Container()
+                        : CmcFilter(
+                            costs: this.costs,
+                            list: widget.allCards,
+                            updateList: (List<dynamic> newList) {
+                              // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
+                              allList['cmc'] = newList.cast<MagicCard>();
+                              _update();
+                            },
+                          ),
+                    !widget.types
+                        ? Container()
+                        : TypeFilter(
+                            types: this.types,
+                            list: widget.allCards,
+                            updateList: (List<dynamic> newList) {
+                              // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
+                              allList['type'] = newList.cast<MagicCard>();
+                              _update();
+                            },
+                          ),
+                    !widget.sets
+                        ? Container()
+                        : SetFilter(
+                            sets: this.sets,
+                            list: widget.allCards,
+                            updateList: (List<dynamic> newList) {
+                              // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
+                              allList['set'] = newList.cast<MagicCard>();
+                              _update();
+                            },
+                          ),
+                    !widget.colors
+                        ? Container()
+                        : ColorFilter(
+                            colors: this.colors,
+                            list: widget.allCards,
+                            updateList: (List<dynamic> newList) {
+                              // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
+                              allList['colors'] = newList.cast<MagicCard>();
+                              _update();
+                            },
+                          ),
+                  ])),
+              Center(
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.0)),
+                    onPressed: () {
+                      setState(() {
+                        this.visible = !this.visible;
+                      });
                     },
-                  ),
-                  !widget.types ? Container() : TypeFilter(
-                    types: this.types,
-                    list: widget.allCards,
-                    updateList: (List<dynamic> newList) {
-                      // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
-                      allList['type'] = newList.cast<MagicCard>();
-                      _update();
-                    },
-                  ),
-                  !widget.sets ? Container() : SetFilter(
-                    sets: this.sets,
-                    list: widget.allCards,
-                    updateList: (List<dynamic> newList) {
-                      // newList.sort((a, b) => int.parse(a.number).compareTo(int.parse(b.number)));
-                      allList['set'] = newList.cast<MagicCard>();
-                      _update();
-                    },
-                  ),
-              ]
-            )
-        ),
-      ],
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.white,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: Row(
+                        children: [
+                          Icon(
+                            !this.visible
+                                ? Icons.keyboard_arrow_down
+                                : Icons.keyboard_arrow_up,
+                            color: Colors.white,
+                            size: ResponsiveSize.responsiveWidth(context, 5),
+                          ),
+                          // Image(
+                          //   image: !this.visible
+                          //       ? AssetImage('assets/images/arrow_down.png')
+                          //       : AssetImage('assets/images/arrow_up.png'),
+                          //
+                          // ),
+                          Text('Filters'),
+                        ],
+                      ),
+                    )),
+              ),
+            ],
+          )),
     );
   }
 }
--- lib/components/filter/TypeFilter.dart
+++ lib/components/filter/TypeFilter.dart
@@ -1,4 +1,5 @@
 import 'package:MagicFlutter/class/MagicCard.dart';
+import 'package:MagicFlutter/utils/ResponsiveSize.dart';
 import 'package:flutter/material.dart';
 import 'package:MagicFlutter/components/filter/Filter.dart';
 
@@ -27,7 +28,11 @@ class TypeFilterState extends State<TypeFilter> {
       filterValues: widget.types,
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       buildItem: (dynamic value, int i) {
-        return Text(value);
+        return Image(
+            image: AssetImage('assets/images/types/' + value.toLowerCase() + '.png'),
+            width: ResponsiveSize.responsiveWidth(context, 7),
+            height: ResponsiveSize.responsiveWidth(context, 7),
+            color: Colors.black);
       },
       list: widget.list,
       condition: (dynamic value, dynamic item) {
